2c717e1552e9787df4b5bf255c2cab8bd8e92508
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-07 01:30:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,3 +14,9 @@ def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase 
 end
+
+def all_phrases
+  puts "It's-a me, Mario!"
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts "It's Dangerous To Go Alone! Take This."
+end
